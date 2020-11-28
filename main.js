e9b5e7a654431131772a016f465b7b193e06561c
Merry  HA <69305320+MerryHa@users.noreply.github.com>
2020-11-28 21:32:57 +0900

    JS: PopUp

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ const gameBtn=document.querySelector('.game__button');
 const gameTimer=document.querySelector('.game__timer');
 const gameScore=document.querySelector('.game__score');
 
+const popUp=document.querySelector('.pop-up');
+const popUpText=document.querySelector('.pop-up__message');
+const popUpRefresh=document.querySelector('.pop-up__refresh');
+
 let started=false;
 let score=0;
 let timer=undefined; //🌟🌟🌟
@@ -30,28 +34,32 @@ function startGame(){
     showTimerAndScore();
     startGameTimer();
 }
-// function stopGame(){
-//     hidePlayBtn();
-//     stopGameTimer();
-// }
-// function hidePlayBtn(){
-    
-// }
-// function stopTimer(){
+function stopGame(){
+    hideGameBtn();
+    stopGameTimer();
+    showPopUpWithText('Replay❓');
+}
 
-// }
+function showPopUpWithText(text){
+    popUp.classList.remove('pop-up--hide');
+    popUpText.textContent=text;
+}
 function startGameTimer(){
     let remainingTimeSec=GAME_DURATION_SEC;
     updateTimerText(remainingTimeSec);
     timer=setInterval(() => {
         if(remainingTimeSec<=0){
             clearInterval(timer);
-        
+            started=false;
+            stopGame();
             return;
         }else{
             updateTimerText(--remainingTimeSec);
         }
     }, 1000);
+}
+function stopGameTimer(){
+    clearInterval(timer);
 }
 function updateTimerText(time){
     let minutes=Math.floor(time/60);
@@ -67,6 +75,9 @@ function showStopBtn(){
     const icon=gameBtn.querySelector('.fa-play');
     icon.classList.add('fa-stop');
     icon.classList.remove('fa-play');
+}
+function hideGameBtn(){
+    gameBtn.style.visibility='hidden';
 }
 function initGame(){
     //당근과 벌레를 생성해서 랜덤으로 추가한다
